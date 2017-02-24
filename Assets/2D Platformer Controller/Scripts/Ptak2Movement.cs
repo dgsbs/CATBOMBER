64980aa873578e845fb1d79b860bba7eabe9ff24
dgsbs <dymel@o2.pl>
2017-02-24 22:24:03 +0100

    CATBOMBER

---
+++ Assets/2D Platformer Controller/Scripts/Ptak2Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ptak2Movement : MonoBehaviour {
+    private const float MoveValue = 0.07f;
+    public GameObject ptak;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.ptak = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 newPosition = this.ptak.transform.position;
+            newPosition.x = newPosition.x + Ptak2Movement.MoveValue;
+            this.ptak.transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 newPosition = this.ptak.transform.position;
+            newPosition.x = newPosition.x - Ptak2Movement.MoveValue;
+            this.ptak.transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 newPosition = this.ptak.transform.position;
+            newPosition.y = newPosition.y - Ptak2Movement.MoveValue;
+            this.ptak.transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 newPosition = this.ptak.transform.position;
+            newPosition.y = newPosition.y + Ptak2Movement.MoveValue;
+            this.ptak.transform.position = newPosition;
+        }
+    }
+}
---
+++ Assets/2D Platformer Controller/Scripts/PtakMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PtakMovement : MonoBehaviour
+{
+    private const float MoveValue = 0.07f;
+    public GameObject ptak;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.ptak = gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKey(KeyCode.RightArrow))
+	    {
+            Vector3 newPosition = this.ptak.transform.position;
+            newPosition.x = newPosition.x + PtakMovement.MoveValue;
+            this.ptak.transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 newPosition = this.ptak.transform.position;
+            newPosition.x = newPosition.x - PtakMovement.MoveValue;
+            this.ptak.transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 newPosition = this.ptak.transform.position;
+            newPosition.y = newPosition.y - PtakMovement.MoveValue;
+            this.ptak.transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 newPosition = this.ptak.transform.position;
+            newPosition.y = newPosition.y + PtakMovement.MoveValue;
+            this.ptak.transform.position = newPosition;
+        }
+    }
+}
