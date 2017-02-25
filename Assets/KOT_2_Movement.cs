043f0178090aa8fc2078073c84266448923be8f7
dgsbs <dymel@o2.pl>
2017-02-25 02:59:49 +0100

    Dwa koty srają i się zabijają

--- Assets/KOT_2_Movement.cs
+++ Assets/KOT_2_Movement.cs
@@ -4,12 +4,15 @@ public class KOT_2_Movement : MonoBehaviour
 {
     private const float MoveValue = 0.07f;
     public GameObject ptak;
+    public GameObject Bomb;
+    private float nextBombTimeSpan = 0.5f;
+    private float nextBombTime;
 
     // Use this for initialization
     void Start()
     {
         this.ptak = gameObject;
-        
+        this.nextBombTime = 0;
     }
 
     // Update is called once per frame
@@ -20,24 +23,37 @@ public class KOT_2_Movement : MonoBehaviour
             Vector3 newPosition = this.ptak.transform.position;
             newPosition.x = newPosition.x + KOT_2_Movement.MoveValue;
             this.ptak.transform.position = newPosition;
+            GetComponent<Animator>().Play("cat_black_right");
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             Vector3 newPosition = this.ptak.transform.position;
             newPosition.x = newPosition.x - KOT_2_Movement.MoveValue;
             this.ptak.transform.position = newPosition;
+            GetComponent<Animator>().Play("cat_black_left");
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
             Vector3 newPosition = this.ptak.transform.position;
             newPosition.y = newPosition.y - KOT_2_Movement.MoveValue;
             this.ptak.transform.position = newPosition;
+            GetComponent<Animator>().Play("cat_black_down");
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        else if (Input.GetKey(KeyCode.UpArrow))
         {
             Vector3 newPosition = this.ptak.transform.position;
             newPosition.y = newPosition.y + KOT_2_Movement.MoveValue;
             this.ptak.transform.position = newPosition;
+            GetComponent<Animator>().Play("cat_black_up");
+        }
+
+        if (Input.GetKey(KeyCode.Keypad0))
+        {
+            if (Time.time > this.nextBombTime)
+            {
+                this.nextBombTime = Time.time + this.nextBombTimeSpan;
+                Instantiate(this.Bomb, transform.position, Quaternion.identity);
+            }
         }
     }
 }
